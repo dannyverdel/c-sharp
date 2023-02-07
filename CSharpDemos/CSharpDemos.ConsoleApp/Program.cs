f5f8ea2e6ef96abb9bef0a1d2f8f9a98e436adbc
Danny Verdel <dannyverdel@Dannys-MacBook-Pro.local>
2023-02-07 14:29:14 +0100

    learned about the prototype design pattern

--- CSharpDemos/CSharpDemos.ConsoleApp/Program.cs
+++ CSharpDemos/CSharpDemos.ConsoleApp/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CSharpDemos.ClassLibrary;
 
-IInvokeMethod im = new CSharpDemos.ClassLibrary.DesignPatterns.BuilderPattern.Builder.WrongBuilder.InvokeWrongBuilder();
+IInvokeMethod im = new CSharpDemos.ClassLibrary.DesignPatterns.PrototypePattern.PrototypeExcelExample.InvokePrototypeExcelExample();
 
 im.InvokeMethod();
 Console.ReadLine();
