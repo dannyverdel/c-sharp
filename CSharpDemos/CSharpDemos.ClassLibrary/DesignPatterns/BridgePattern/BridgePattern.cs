6eb5a352facb50276a00ece613d044775029f80e
Danny Verdel <dannyverdel@Dannys-MacBook-Pro.local>
2023-02-14 13:37:48 +0100

    formatting

--- CSharpDemos/CSharpDemos.ClassLibrary/DesignPatterns/BridgePattern/BridgePattern.cs
+++ CSharpDemos/CSharpDemos.ClassLibrary/DesignPatterns/BridgePattern/BridgePattern.cs
@@ -1,25 +1,25 @@
 ﻿using System;
 namespace CSharpDemos.ClassLibrary.DesignPatterns.BridgePattern
 {
-	public class InvokeBridgePattern : IInvokeMethod
-	{
-        /*
-         * The Bridge design pattern is a structural design pattern that separates the abstraction (interface) from its implementation, 
-         * so that they can be developed independently and can be changed dynamically at runtime.
-         * 
-         * In simple terms, the Bridge design pattern allows you to create an interface that can have multiple implementations, which can be switched out dynamically. 
-         * This means that you can change the behavior of your system by swapping out the implementation of an interface, without changing the code that uses that interface.
-         * 
-         * With this example, you can create multiple implementations of IWeapon (Bow and Explosives), 
-         * and you can switch the implementation of IWeapon dynamically by changing the constructor of the Mob objects.
-         * 
-         * You can see that the Skeleton is given a bow, while the Creeper is given explosives, 
-         * because they were constructed with different implementations of IWeapon. 
-         * You can also change the implementation of the weapon dynamically by creating a new instance of the Mob class with a different IWeapon implementation.
-         */
+    /*
+    * The Bridge design pattern is a structural design pattern that separates the abstraction (interface) from its implementation, 
+    * so that they can be developed independently and can be changed dynamically at runtime.
+    * 
+    * In simple terms, the Bridge design pattern allows you to create an interface that can have multiple implementations, which can be switched out dynamically. 
+    * This means that you can change the behavior of your system by swapping out the implementation of an interface, without changing the code that uses that interface.
+    * 
+    * With this example, you can create multiple implementations of IWeapon (Bow and Explosives), 
+    * and you can switch the implementation of IWeapon dynamically by changing the constructor of the Mob objects.
+    * 
+    * You can see that the Skeleton is given a bow, while the Creeper is given explosives, 
+    * because they were constructed with different implementations of IWeapon. 
+    * You can also change the implementation of the weapon dynamically by creating a new instance of the Mob class with a different IWeapon implementation.
+    */
 
+    public class InvokeBridgePattern : IInvokeMethod
+    {
         public void InvokeMethod()
-		{
+        {
             IWeapon skeleton_weapon = new Bow();
             IWeapon creeper_weapon = new Explosives();
 
