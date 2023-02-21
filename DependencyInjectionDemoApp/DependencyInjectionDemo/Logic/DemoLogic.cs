822126803714f03f2ab21b31487529571348e008
Danny Verdel <dannyverdel@Dannys-MacBook-Pro.local>
2023-02-21 23:33:10 +0100

    learned about dependency injection in blazor

---
+++ DependencyInjectionDemoApp/DependencyInjectionDemo/Logic/DemoLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace DependencyInjectionDemo.Logic;
+
+public class DemoLogic : IDemoLogic
+{
+    public int Value1 { get; private set; }
+    public int Value2 { get; private set; }
+
+    private readonly ILogger<DemoLogic> _logger;
+
+    public DemoLogic(ILogger<DemoLogic> logger)
+    {
+        Value1 = Random.Shared.Next(1, 1001);
+        Value2 = Random.Shared.Next(1, 1001);
+
+        _logger = logger;
+        _logger.LogInformation("Instantiated class");
+    }
+}
+
