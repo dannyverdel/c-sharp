3b826739b1aec2a169b5373dc0ddaff1a41228cb
Danny Verdel <dannyverdel@Dannys-MacBook-Pro.local>
2023-02-22 21:48:15 +0100

    .

--- CachingExampleApp/DataAccessLibrary/EmployeeModel.cs
+++ CachingExampleApp/DataAccessLibrary/EmployeeModel.cs
@@ -2,7 +2,8 @@
 
 public class EmployeeModel
 {
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
+				public string FirstName { get; set; } = string.Empty;
+				public string LastName { get; set; } = string.Empty;
+				public int Age { get; set; }
 }
 
--- CachingExampleApp/DataAccessLibrary/SampleDataAccess.cs
+++ CachingExampleApp/DataAccessLibrary/SampleDataAccess.cs
@@ -1,57 +1,45 @@
 ﻿using System;
 using Bogus;
 using Microsoft.Extensions.Caching.Memory;
-namespace DataAccessLibrary
+
+namespace DataAccessLibrary;
+
+public class SampleDataAccess
 {
-    public class SampleDataAccess
-    {
-        private readonly IMemoryCache _memory_cache;
-        public SampleDataAccess(IMemoryCache memory_cache) => _memory_cache = memory_cache;
+				private readonly IMemoryCache _memory_cache;
+				public SampleDataAccess(IMemoryCache memory_cache) => _memory_cache = memory_cache;
 
-        public List<EmployeeModel> GetEmployees()
-        {
-            List<EmployeeModel> output = new EmployeeFaker().Generate(100);
+				public async Task<List<EmployeeModel>> GetEmployeesAsync() {
+								List<EmployeeModel> output = new EmployeeFaker().Generate(Random.Shared.Next(500, 10001));
 
-            Thread.Sleep(3000);
+								await Task.Delay(3000);
 
-            return output;
-        }
+								return output;
+				}
 
-        public async Task<List<EmployeeModel>> GetEmployeesAsync()
-        {
-            List<EmployeeModel> output = new EmployeeFaker().Generate(100);
+				public async Task<List<EmployeeModel>> GetEmployeesCache() {
+								List<EmployeeModel>? output;
 
-            await Task.Delay(3000);
+								output = _memory_cache.Get<List<EmployeeModel>>("employees");
 
-            return output;
-        }
+								if ( output is null ) {
+												output = await GetEmployeesAsync();
+												_memory_cache.Set("employees", output, TimeSpan.FromMinutes(1));
+								}
 
-        public async Task<List<EmployeeModel>> GetEmployeesCache()
-        {
-            List<EmployeeModel>? output;
+								return output;
+				}
+}
 
-            output = _memory_cache.Get<List<EmployeeModel>>("employees");
+public class EmployeeFaker : Faker<EmployeeModel>
+{
+				public EmployeeFaker() {
+								Random rnd = new Random();
 
-            if (output is null)
-            {
-                output = await GetEmployeesAsync();
-                _memory_cache.Set("employees", output, TimeSpan.FromMinutes(1));
-            }
-
-            return output;
-        }
-    }
-
-    public class EmployeeFaker : Faker<EmployeeModel>
-    {
-        public EmployeeFaker()
-        {
-            Random rnd = new Random();
-
-            UseSeed(rnd.Next(1000))
-            .RuleFor(x => x.FirstName, ef => ef.Person.FirstName)
-            .RuleFor(x => x.LastName, ef => ef.Person.LastName);
-        }
-    }
+								UseSeed(Random.Shared.Next(1, 1000001))
+								.RuleFor(x => x.FirstName, ef => ef.Person.FirstName)
+								.RuleFor(x => x.LastName, ef => ef.Person.LastName)
+								.RuleFor(x => x.Age, _ => Random.Shared.Next(20, 76));
+				}
 }
 
