261a00f59c40043b9471b137462210453182f0b8
Danny Verdel <d.verdel@verdel.nl>
2025-02-24 10:12:53 +0100

    Azure app configuration demo

---
+++ AppConfigDemo/AppConfigDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.FeatureManagement;
+
+namespace AppConfigDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args) {
+            var builder = new ConfigurationBuilder();
+
+            builder.AddAzureAppConfiguration(options => {
+                options.Connect("MyConnectionString").UseFeatureFlags();
+            });
+
+            var config = builder.Build();
+
+            Console.WriteLine(config["TestApp:Settings:Message"] ?? "Hello world!");
+
+            var featureManager = new FeatureManager(new ConfigurationFeatureDefinitionProvider(config));
+
+            // Feature flag
+            if ( await featureManager.IsEnabledAsync("Beta") ) {
+                Console.WriteLine("Beta feature is enabled");
+            } else {
+                Console.WriteLine("Beta feature is disabled");
+            }
+
+            // Variant feature flag
+            if ( await featureManager.IsEnabledAsync("Greeting") ) {
+                Console.WriteLine("Variant feature is enabled");
+                Variant greetingVariant = await featureManager.GetVariantAsync("Greeting")!;
+                Console.WriteLine($"You are working with the feature variant {greetingVariant.Name}");
+
+            } else {
+                Console.WriteLine("Variant feature is disabled");
+            }
+        }
+    }
+}
