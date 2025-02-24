f5f4ed99c260b5693f16c38395ef8d600b914396
Danny Verdel <d.verdel@verdel.nl>
2025-02-24 10:29:17 +0100

    Azure key vault demo

---
+++ KeyVaultDemo/KeyVaultDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+namespace KeyVaultDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args) {
+            string keyVaultUir = "myKeyVaultUri";
+
+            SecretClient secretClient = new(new Uri(keyVaultUir), new DefaultAzureCredential());
+
+            await secretClient.SetSecretAsync("connectionString", "myConnectionString");
+
+            KeyVaultSecret secret = await secretClient.GetSecretAsync("connectionString");
+            Console.WriteLine(secret.Name + ": " + secret.Value);
+
+            DeleteSecretOperation? deleteSecretOperation = await secretClient.StartDeleteSecretAsync("connectionString");
+            await deleteSecretOperation.WaitForCompletionAsync();
+            await secretClient.PurgeDeletedSecretAsync("connectionString");
+        }
+    }
+}
